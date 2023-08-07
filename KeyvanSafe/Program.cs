543ab92d86c6321786da4f307b10aff2efe7e2db
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-08-07 10:02:41 -0700

    config program.cs

--- KeyvanSafe/Program.cs
+++ KeyvanSafe/Program.cs
@@ -1,12 +1,23 @@
+using Dayana.Server.Api.Extensions.Middleware;
+using KeyvanSafe.Server.Configs;
+
+#region builder part
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
 
+// Add services to the container.
+builder.Services.AddServices(configuration);
 builder.Services.AddControllers();
+builder.Services.AddMvc();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion
+
+#region app part
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,11 +26,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseConfiguredExceptionHandler(environment);
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseRouting();
 app.Run();
+
+#endregion
