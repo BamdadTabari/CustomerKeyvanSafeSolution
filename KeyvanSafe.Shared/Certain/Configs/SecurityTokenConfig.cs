7257101ffa1b666d138c067f339359d9424b400b
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-08-08 14:13:22 -0700

    .

--- KeyvanSafe.Shared/Certain/Configs/SecurityTokenConfig.cs
+++ KeyvanSafe.Shared/Certain/Configs/SecurityTokenConfig.cs
@@ -8,6 +8,6 @@ public class SecurityTokenConfig
     public string Audience { get; set; } = "https://localhost";
     public string AccessTokenSecretKey { get; set; } = "WQ7+dPhLEHdhdaKNzu!ck-fg86TPhUfd#E&&Qq+=vUtfxJ!@sDfe#u^prXW2&Qhmy33u!@e?5-xb*";
     public TimeSpan AccessTokenLifetime { get; set; } = TimeSpan.FromDays(7);
-    public string RefreshTokenSecretKey { get; set; } = "WQ7+dPsdjifdsklhFBNzu!ck-fg86TPhUAPOSIDPIOUFDIOshF#u^prXW2&Qhmy33u!@e5455sdfd";
+    public string RefreshTokenSecretKey { get; set; } = "WQ7+dPsdjifdsklhFBNzudsifpifsiPhUAPOSIDPIOUFDIOshF#u^prXW2&Qhmy33u!@e5455sdfd";
     public TimeSpan RefreshTokenLifetime { get; set; } = TimeSpan.FromDays(7);
 }
