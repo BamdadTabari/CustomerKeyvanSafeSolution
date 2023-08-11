f5aaa06103f378937d6845b2483989dd889fc906
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-08-11 11:59:54 -0700

    htttp/ user filter

--- KeyvanSafe.Shared/Persistence/Extensions/Identity/UserQueryableExtension.cs
+++ KeyvanSafe.Shared/Persistence/Extensions/Identity/UserQueryableExtension.cs
@@ -1,33 +1,37 @@
-﻿namespace KeyvanSafe.Shared.Persistence.Extensions.Identity;
+﻿using KeyvanSafe.Shared.Certain.Enums;
+using KeyvanSafe.Shared.EntityFramework.Entities.Identity.Users;
+using KeyvanSafe.Shared.Infrastructure.Pagination;
+
+namespace KeyvanSafe.Shared.Persistence.Extensions.Identity;
 
 public static class UserQueryableExtension
 {
-    public static IQueryable<User> ApplyFilter(this IQueryable<User> query, CustomaizedPaginationFilterTwo<List<UserState>?, UserSortBy?> filter)
+    public static IQueryable<User> ApplyFilter(this IQueryable<User> query, DefaultPaginationFilter filter)
     {
-        // Filter by keyword
-        if (!string.IsNullOrEmpty(filter.keyword))
+        // Filter by uSERNAME
+        if (!string.IsNullOrEmpty(filter.Name))
             query = query.Where(x =>
-                x.Username.ToLower().Contains(filter.keyword.ToLower().Trim()));
+                x.Username.ToLower().Contains(filter.Name.ToLower().Trim()));
 
         // Filter by email
         if (!string.IsNullOrEmpty(filter.StringValue))
             query = query.Where(x => x.Email.ToLower().Contains(filter.StringValue.ToLower().Trim()));
 
-        // Filter by statuses
-        if (filter.Value1?.Any() == true)
-            query = query.Where(x => filter.Value1.Contains(x.State));
+        // Filter by Mobile
+        if (!string.IsNullOrEmpty(filter.Keyword))
+            query = query.Where(x => x.Mobile.ToLower().Contains(filter.Keyword.ToLower().Trim()));
 
         return query;
     }
 
-    public static IQueryable<User> ApplySort(this IQueryable<User> query, UserSortBy? sortBy)
+    public static IQueryable<User> ApplySort(this IQueryable<User> query, UserSortByEnum? sortBy)
     {
         return sortBy switch
         {
-            UserSortBy.CreationDate => query.OrderBy(x => x.CreatedAt),
-            UserSortBy.CreationDateDescending => query.OrderByDescending(x => x.CreatedAt),
-            UserSortBy.LastLoginDate => query.OrderBy(x => x.LastLoginDate),
-            UserSortBy.LastLoginDateDescending => query.OrderByDescending(x => x.LastLoginDate),
+            UserSortByEnum.CreationDate => query.OrderBy(x => x.CreatedAt),
+            UserSortByEnum.CreationDateDescending => query.OrderByDescending(x => x.CreatedAt),
+            UserSortByEnum.LastLoginDate => query.OrderBy(x => x.LastLoginDate),
+            UserSortByEnum.LastLoginDateDescending => query.OrderByDescending(x => x.LastLoginDate),
             _ => query.OrderByDescending(x => x.Id)
         };
     }
---
+++ KeyvanSafe.Shared/Persistence/HttpObjects/IHttpService.cs
@@ -0,0 +1,12 @@
+﻿using KeyvanSafe.Shared.Infrastructure.Pagination;
+
+namespace KeyvanSafe.Shared.Persistence.HttpObjects;
+public interface IHttpService
+{
+    Task<List<T>> GetValueList<T>(string requestUrl);
+    Task<T> GetValue<T>(string requestUrl);
+    Task<HttpResponseMessage> PostValue<T>(string requestUrl, T data);
+    Task<HttpResponseMessage> PutValue<T>(string requestUrl, T data);
+    Task<HttpResponseMessage> DeleteValue<T>(string requestUrl);
+    Task<PaginatedList<T>> GetPagedValue<T>(string requestUrl, DefaultPaginationFilter defaultPaginationFilter);
+}
