625ba124604cb65474d494943ecb0830d3592724
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-08-12 08:51:44 -0700

    sdafa

--- KeyvanSafe.Shared/Infrastructure/Errors/GenericErrors.cs
+++ KeyvanSafe.Shared/Infrastructure/Errors/GenericErrors.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace KeyvanSafe.Shared.Infrastructure.Errors;
-public static class GenericErrors<>
+public static class GenericErrors
 {
+    public static string Message { get; set; }
+    public static int ErrorCode { get; set; }
 }
